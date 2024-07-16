61f1f57a446340993154e495d6b2acd1ac534498
rduo1009 <robertduong09alt@gmail.com>
2024-07-16 17:20:25 +1000

    pronoun unittest

--- tests/test_noun.py
+++ tests/test_noun.py
@@ -2,6 +2,7 @@ import sys, os  # noqa: E401
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+import pytest
 from grammatica.endings import Noun
 
 
---
+++ tests/test_pronoun.py
@@ -0,0 +1,10 @@
+import sys, os  # noqa: E401
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import pytest
+from grammatica.endings import Pronoun
+from grammatica.edge_cases import PRONOUNS
+
+def test_pronoun():
+    assert Pronoun(pronoun="hic", meaning="this").endings == PRONOUNS["hic"]
