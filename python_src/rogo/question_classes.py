1dc71ff9adf637dfac5fcafb21acb19cebd62319
rduo1009 <robertduong09alt@gmail.com>
2024-08-13 10:33:25 +1000

    more question classes; running importfmt

--- python_src/rogo/question_classes.py
+++ python_src/rogo/question_classes.py
@@ -3,8 +3,9 @@
 
 """Contains representations of questions about Latin vocabulary."""
 
-from .. import accido
 from dataclasses import dataclass
+
+from .. import accido
 
 
 # TI = type-in
@@ -12,15 +13,19 @@ from dataclasses import dataclass
 # PW = parse word
 
 
-@dataclass
 class _Question:
-    question: str
+    """Base class for questions."""
+
+    pass
 
 
 @dataclass
 class TI_EngtoLat_Question(_Question):
-    """A question that asks for the Latin translation of an English word."""
+    """A question that asks for the Latin translation of an English word.
+    For example, "I search" (answer: "quaero")
+    """
 
+    english: str
     answer: accido.misc.Ending
 
     def check(self, response: str, further_answers: list[str]) -> bool:
@@ -35,8 +40,10 @@ class TI_EngtoLat_Question(_Question):
 
 @dataclass
 class TI_LattoEng_Question(_Question):
-    """A question that asks for the English translation of a Latin word."""
+    """A question that asks for the English translation of a Latin word.
+    For example, "quaero" (answer: "I search")"""
 
+    latin: str
     answer: accido.misc.Meaning
 
     def check(self, response: str, further_answers: list[str]) -> bool:
@@ -49,3 +56,35 @@ class TI_LattoEng_Question(_Question):
             return True
         else:
             raise TypeError(f"Unknown type of answer: {type(self.answer)}")
+
+
+@dataclass
+class PW_ComptoLat_Question(_Question):
+    """A question that asks for a Latin word given the grammatical
+    components of the word.
+    For example:
+    present active indicative 2nd person plural of "quaero"
+    (answer: "quaeratis")"""
+
+    latin: str
+    components: accido.endings.EndingComponents
+    answer: str
+
+    def check(self, response: str) -> bool:
+        return response == self.answer
+
+
+@dataclass
+class PW_LattoComp_Question(_Question):
+    """A question that asks for the grammatical components of a Latin word,
+    given the word.
+    For example:
+    Parse "quaeratis" (hear: quaero, quaerere, quaesivi, quaesitus)
+    (answer: "present active indicative 2nd person plural")"""
+
+    dictionary_entry: str
+    latin: str
+    answer: accido.endings.EndingComponents
+
+    def check(self, response: accido.endings.EndingComponents) -> bool:
+        return response == self.answer
