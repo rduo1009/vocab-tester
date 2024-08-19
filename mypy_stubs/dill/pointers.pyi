e6526d681511838e16671ed1d115ef82c5758acd
rduo1009 <robertduong09alt@gmail.com>
2024-08-19 10:34:46 +1000

    fixing more stubs
    (cherry picked from commit 7831cfb681abe66e9672e3c4961fb5ec18a27e93)

--- mypy_stubs/dill/pointers.pyi
+++ mypy_stubs/dill/pointers.pyi
@@ -1,12 +1,24 @@
 from __future__ import annotations
 
+from typing import Any
+
 from ._dill import _locate_object as at  # type: ignore
 from ._dill import _proxy_helper as reference  # type: ignore
 
 __all__ = ["parent", "reference", "at", "parents", "children"]
 
-def parent(obj, objtype, ignore=()): ...
-def parents(obj, objtype, depth: int = 1, ignore=()): ...
-def children(obj, objtype, depth: int = 1, ignore=()): ...
+def parent(obj: object, objtype: type, ignore: tuple = ()) -> Any: ...  # type: ignore
+def parents(
+    obj: object,
+    objtype: type,
+    depth: int = 1,
+    ignore: tuple = (),  # type: ignore
+) -> Any: ...
+def children(
+    obj: object,
+    objtype: type,
+    depth: int = 1,
+    ignore: tuple = (),  # type: ignore
+) -> Any: ...
 
 refobject = at
