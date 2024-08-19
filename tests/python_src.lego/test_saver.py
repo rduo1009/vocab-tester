aa85a8caff62c67153a407ae3c211f8bd08fd3f9
rduo1009 <robertduong09alt@gmail.com>
2024-08-19 21:54:27 +1000

    improving tests
    (cherry picked from commit 777d17a3cd5fbaf1bf52c2ae47087b00f3447549)

--- tests/python_src.lego/test_saver.py
+++ tests/python_src.lego/test_saver.py
@@ -1,18 +1,23 @@
-# fmt: off
-# mypy: ignore-errors
-# ruff: noqa
-
-import sys, os  # noqa: E401
+import os
+import sys  # noqa: E401
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 
-import pytest
 from pathlib import Path
 
+import pytest  # type: ignore[import-not-found]
 from python_src import lego
+
 
 def test_saver():
     x = lego.reader.read_vocab_file(Path("tests/python_src.lego/test_vocab_files/regular_list.txt"))
     lego.saver.save_vocab_dump(Path("tests/python_src.lego/test_vocab_files/testdump/regular_list.testdump"), x)
     y = lego.reader.read_vocab_dump(Path("tests/python_src.lego/test_vocab_files/testdump/regular_list.testdump"))
     assert x == y
+
+
+def test_nodirectory():
+    x = lego.reader.read_vocab_file(Path("tests/python_src.lego/test_vocab_files/regular_list.txt"))
+    with pytest.raises(FileNotFoundError) as error:
+        lego.saver.save_vocab_dump(Path("tests/python_src.lego/test_vocab_filesajofsdifhbd/testdump/regular_list.testdump"), x)
+    assert "The directory tests/python_src.lego/test_vocab_filesajofsdifhbd/testdump does not exist." == str(error.value)
--- tests/run_all_tests.py
+++ tests/run_all_tests.py
@@ -1,14 +1,25 @@
+# mypy: ignore-errors
+
 import importlib
+import inspect
 import os
 import sys
 import warnings
 
 warnings.filterwarnings("ignore")
 
+
+def run_tests_in_class(cls):
+    instance = cls()
+
+    for name, func in inspect.getmembers(cls, predicate=inspect.isfunction):
+        if name.startswith("test_"):
+            func(instance)
+            # print(f"Running {cls.__name__}.{test_func.__name__}...")
+
+
 if __name__ == "__main__":
-    sys.path.insert(
-        0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-    )
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
     test_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -28,3 +39,6 @@ if __name__ == "__main__":
                         if callable(test_func):
                             # print(f"Running {test_func.__name__}...")
                             test_func()
+                    elif attr_name.startswith("Test"):
+                        cls = getattr(module, attr_name)
+                        run_tests_in_class(cls)
