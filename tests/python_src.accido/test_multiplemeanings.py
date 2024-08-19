faaac8e2e1f48ff9e502201396f5903069f6bbf5
rduo1009 <robertduong09alt@gmail.com>
2024-08-19 21:54:27 +1000

    improving tests
    (cherry picked from commit 777d17a3cd5fbaf1bf52c2ae47087b00f3447549)

---
+++ tests/python_src.accido/test_multiplemeanings.py
@@ -0,0 +1,12 @@
+import os
+import sys  # noqa: E401
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+import pytest  # type: ignore[import-not-found]
+from python_src.accido.misc import MultipleMeanings
+
+
+def test_repr():
+    multiple_endings = MultipleMeanings(["a", "b", "c"])
+    assert repr(multiple_endings) == "MultipleMeanings(a, b, c)"
