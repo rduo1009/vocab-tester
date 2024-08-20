458e8b9b1b8cc587f3a3724759975ae3a878c431
rduo1009 <robertduong09alt@gmail.com>
2024-08-20 16:39:43 +1000

    🔨 refactor: putting transfero functions into own file

--- python_src/transfero/words.py
+++ python_src/transfero/words.py
@@ -1,85 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import lemminflect
-from inflect import engine
+"""Contains functions that inflect English words."""
 
-from .. import accido
+from __future__ import annotations
 
-pluralinflect = engine()  # To distinguish from the lemminflect module
-del engine
-
-
-def _get_possessive(noun: str) -> str:
-    """Return the possessive form of a given noun."""
-    if noun.endswith("s"):
-        return noun + "'"
-    else:
-        return noun + "'s"
-
-
-def find_noun_inflections(
-    noun: str, components: accido.misc.EndingComponents
-) -> set[str]:
-    # Something has gone very wrong here if this happens, but just to be safe
-    if not (hasattr(components, "case") and hasattr(components, "number")):
-        raise ValueError("Case and number must be specified")
-
-    noun_lemma = lemminflect.getLemma(noun, "NOUN")[0]
-    base_forms: set[str] = set()
-
-    match components.number:
-        case "singular":
-            base_forms = {lemminflect.getInflection(noun_lemma, "NN")[0]}
-
-        case "plural":
-            base_forms.add(pluralinflect.plural_noun(noun_lemma))
-            pluralinflect.classical(all=True)
-            base_forms.add(pluralinflect.plural_noun(noun_lemma))
-            pluralinflect.classical(all=False)
-
-        case _:
-            raise ValueError(f"Number '{components.number}' not recognised")
-
-    match components.case:
-        case "nominative" | "vocative" | "accusative" | "ablative":
-            return base_forms
-
-        case "genitive":
-            possessive_genitive = {
-                _get_possessive(base_form) for base_form in base_forms
-            }
-
-            if components.number == "singular":
-                return possessive_genitive | {
-                    f"of the {base_form}" for base_form in base_forms
-                } | {
-                    pluralinflect.inflect(f"of a('{base_form}')") for base_form in base_forms
-                }  # fmt: skip
-
-            return possessive_genitive | {
-                f"of the {base_form}" for base_form in base_forms
-            }
-
-        case "dative":
-            if components.number == "singular":
-                return {
-                    f"for the {base_form}" for base_form in base_forms
-                } | {
-                    pluralinflect.inflect(f"for a('{base_form}')") 
-                    for base_form in base_forms
-                } | {
-                    f"to the {base_form}" for base_form in base_forms
-                } | {
-                    pluralinflect.inflect(f"to a('{base_form}')") 
-                    for base_form in base_forms
-                }  # fmt: skip
-
-            return {
-                f"for the {base_form}" for base_form in base_forms
-            } | {
-                f"to the {base_form}" for base_form in base_forms
-            }  # fmt: skip
-
-        case _:
-            raise ValueError(f"Case '{components.case}' not recognised")
+from .noun_inflection import find_noun_inflections as find_noun_inflections
