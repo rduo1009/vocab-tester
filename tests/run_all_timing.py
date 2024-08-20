acc99042f4af59051063222be3d84c2b87609c46
rduo1009 <robertduong09alt@gmail.com>
2024-08-20 16:34:53 +1000

    adding timing
    (cherry picked from commit 8a763d23b9ba320462ac610d675e26864c140f9c)
    (cherry picked from commit 2438049f52f06e1b6faf3c0d70cd5f55b4b5fd1d)

---
+++ tests/run_all_timing.py
@@ -0,0 +1,33 @@
+# mypy: ignore-errors
+
+import importlib
+import inspect
+import os
+import sys
+import warnings
+
+warnings.filterwarnings("ignore")
+
+RUN_TIMES = 100_000
+
+if __name__ == "__main__":
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+
+    for dirpath, _, filenames in os.walk(test_dir):
+        for filename in filenames:
+            if filename.startswith("timing_") and filename.endswith(".py"):
+                if dirpath not in sys.path:
+                    sys.path.append(dirpath)
+
+                module_name = filename[:-3]
+                module = importlib.import_module(module_name)
+
+                for attr_name in dir(module):
+                    if attr_name.startswith("time_"):
+                        timing_func = getattr(module, attr_name)
+
+                        if callable(timing_func):
+                            # print(f"Running {timing_func.__name__}...")
+                            timing_func(RUN_TIMES)
