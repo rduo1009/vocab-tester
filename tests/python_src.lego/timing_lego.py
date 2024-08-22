8c6d9a297635725d8f3f9e2ed70d8a501b555c98
rduo1009 <robertduong09alt@gmail.com>
2024-08-22 15:48:13 +1000

    🐛 fix: fixing timing bug, timing data for 0.2.4
    (cherry picked from commit af045df070fc4dc69ea1fd319b8d8c37025d818e)

--- tests/python_src.lego/timing_lego.py
+++ tests/python_src.lego/timing_lego.py
@@ -5,7 +5,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 
 import python_src
 from codetiming import Timer
-from humanfriendly import format_timespan
 from python_src.lego.misc import VocabList
 
 l: VocabList
