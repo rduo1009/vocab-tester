522254ce78a24eae6edd1d881042e0346a345198
rduo1009 <robertduong09alt@gmail.com>
2024-08-26 23:14:30 +1000

    🐛 fix: actually having init.py in transfero

--- python_src/transfero/__init__.py
+++ python_src/transfero/__init__.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""A package for finding equivalent English words/phrases for Latin
+words/phrases."""
+
+from . import words
+
+__all__ = ["words"]
