03236614a2e9648ffda8f1b2e6b7fad3d2aea374
rduo1009 <robertduong09alt@gmail.com>
2024-09-05 17:24:54 +1000

    🧹 chore: removing main.py for now

--- main.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-try:
-    from icecream import ic  # type: ignore
-except ImportError:  # Graceful fallback if IceCream isn't installed.
-    ic = lambda *a: None if not a else (a[0] if len(a) == 1 else a)  # noqa: E731
