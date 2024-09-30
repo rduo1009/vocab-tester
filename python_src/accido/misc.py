9a914369e944c56823cf198930cdbec86ec7bc06
rduo1009 <robertduong09alt@gmail.com>
2024-09-30 18:25:40 +1000

    ✨ feat: enums for number, tense, etc. inputs

--- python_src/accido/misc.py
+++ python_src/accido/misc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# __coconut_hash__ = 0x44b981cf
+# __coconut_hash__ = 0x386fd023
 
 # Compiled with Coconut version 3.1.2
 
@@ -61,74 +61,98 @@ else:
 
 from dataclasses import dataclass  #3: from dataclasses import dataclass
 from types import SimpleNamespace  #4: from types import SimpleNamespace
-try:  #5: from typing import Final
-    _coconut_sys_0 = sys  # type: ignore  #5: from typing import Final
-except _coconut.NameError:  #5: from typing import Final
-    _coconut_sys_0 = _coconut_sentinel  #5: from typing import Final
-sys = _coconut_sys  #5: from typing import Final
-if sys.version_info >= (3, 8):  #5: from typing import Final
-    if _coconut.typing.TYPE_CHECKING:  #5: from typing import Final
-        from typing import Final  #5: from typing import Final
-    else:  #5: from typing import Final
-        try:  #5: from typing import Final
-            Final = _coconut.typing.Final  #5: from typing import Final
-        except _coconut.AttributeError as _coconut_imp_err:  #5: from typing import Final
-            raise _coconut.ImportError(_coconut.str(_coconut_imp_err))  #5: from typing import Final
-else:  #5: from typing import Final
-    from typing_extensions import Final  #5: from typing import Final
-if _coconut_sys_0 is not _coconut_sentinel:  #5: from typing import Final
-    sys = _coconut_sys_0  #5: from typing import Final
 
-"""Mapping of number values to their more concise abbreviated forms."""  #7: """Mapping of number values to their more concise abbreviated forms."""
-NUMBER_SHORTHAND = _coconut.dict((("singular", "sg"), ("plural", "pl")))  # type: Final[dict[str, str]]  #8: NUMBER_SHORTHAND: Final[dict[str, str]] = {
-if "__annotations__" not in _coconut.locals():  #8: NUMBER_SHORTHAND: Final[dict[str, str]] = {
-    __annotations__ = {}  # type: ignore  #8: NUMBER_SHORTHAND: Final[dict[str, str]] = {
-__annotations__["NUMBER_SHORTHAND"] = 'Final[dict[str, str]]'  #8: NUMBER_SHORTHAND: Final[dict[str, str]] = {
-
-"""Mapping of tense values to their more concise abbreviated forms."""  #13: """Mapping of tense values to their more concise abbreviated forms."""
-TENSE_SHORTHAND = _coconut.dict((("present", "pre"), ("imperfect", "imp"), ("future", "fut"), ("perfect", "per"), ("pluperfect", "plp")))  # type: Final[dict[str, str]]  #14: TENSE_SHORTHAND: Final[dict[str, str]] = {
-if "__annotations__" not in _coconut.locals():  #14: TENSE_SHORTHAND: Final[dict[str, str]] = {
-    __annotations__ = {}  # type: ignore  #14: TENSE_SHORTHAND: Final[dict[str, str]] = {
-__annotations__["TENSE_SHORTHAND"] = 'Final[dict[str, str]]'  #14: TENSE_SHORTHAND: Final[dict[str, str]] = {
-
-"""Mapping of voice values to their more concise abbreviated forms."""  #23: """Mapping of voice values to their more concise abbreviated forms."""
-VOICE_SHORTHAND = _coconut.dict((("active", "act"), ("passive", "pas")))  # type: Final[dict[str, str]]  #24: VOICE_SHORTHAND: Final[dict[str, str]] = {
-if "__annotations__" not in _coconut.locals():  #24: VOICE_SHORTHAND: Final[dict[str, str]] = {
-    __annotations__ = {}  # type: ignore  #24: VOICE_SHORTHAND: Final[dict[str, str]] = {
-__annotations__["VOICE_SHORTHAND"] = 'Final[dict[str, str]]'  #24: VOICE_SHORTHAND: Final[dict[str, str]] = {
-
-"""Mapping of mood values to their more concise abbreviated forms."""  #29: """Mapping of mood values to their more concise abbreviated forms."""
-MOOD_SHORTHAND = _coconut.dict((("indicative", "ind"), ("infinitive", "inf"), ("imperative", "ipe"), ("subjunctive", "sbj"), ("participle", "ptc")))  # type: Final[dict[str, str]]  #30: MOOD_SHORTHAND: Final[dict[str, str]] = {
-if "__annotations__" not in _coconut.locals():  #30: MOOD_SHORTHAND: Final[dict[str, str]] = {
-    __annotations__ = {}  # type: ignore  #30: MOOD_SHORTHAND: Final[dict[str, str]] = {
-__annotations__["MOOD_SHORTHAND"] = 'Final[dict[str, str]]'  #30: MOOD_SHORTHAND: Final[dict[str, str]] = {
-
-"""Mapping of case values to their more concise abbreviated forms."""  #38: """Mapping of case values to their more concise abbreviated forms."""
-CASE_SHORTHAND = _coconut.dict((("nominative", "nom"), ("vocative", "voc"), ("accusative", "acc"), ("genitive", "gen"), ("dative", "dat"), ("ablative", "abl")))  # type: Final[dict[str, str]]  #39: CASE_SHORTHAND: Final[dict[str, str]] = {
-if "__annotations__" not in _coconut.locals():  #39: CASE_SHORTHAND: Final[dict[str, str]] = {
-    __annotations__ = {}  # type: ignore  #39: CASE_SHORTHAND: Final[dict[str, str]] = {
-__annotations__["CASE_SHORTHAND"] = 'Final[dict[str, str]]'  #39: CASE_SHORTHAND: Final[dict[str, str]] = {
-
-"""Mapping of gender values to their more concise abbreviated forms."""  #48: """Mapping of gender values to their more concise abbreviated forms."""
-GENDER_SHORTHAND = _coconut.dict((("masculine", "m"), ("feminine", "f"), ("neuter", "n")))  # type: Final[dict[str, str]]  #49: GENDER_SHORTHAND: Final[dict[str, str]] = {
-if "__annotations__" not in _coconut.locals():  #49: GENDER_SHORTHAND: Final[dict[str, str]] = {
-    __annotations__ = {}  # type: ignore  #49: GENDER_SHORTHAND: Final[dict[str, str]] = {
-__annotations__["GENDER_SHORTHAND"] = 'Final[dict[str, str]]'  #49: GENDER_SHORTHAND: Final[dict[str, str]] = {
-
-"""Mapping of degree values to their more concise abbreviated forms."""  #55: """Mapping of degree values to their more concise abbreviated forms."""
-DEGREE_SHORTHAND = _coconut.dict((("positive", "pos"), ("comparative", "cmp"), ("superlative", "spr")))  # type: Final[dict[str, str]]  #56: DEGREE_SHORTHAND: Final[dict[str, str]] = {
-if "__annotations__" not in _coconut.locals():  #56: DEGREE_SHORTHAND: Final[dict[str, str]] = {
-    __annotations__ = {}  # type: ignore  #56: DEGREE_SHORTHAND: Final[dict[str, str]] = {
-__annotations__["DEGREE_SHORTHAND"] = 'Final[dict[str, str]]'  #56: DEGREE_SHORTHAND: Final[dict[str, str]] = {
-
-"""Mapping of person values to their more concise abbreviated forms."""  #62: """Mapping of person values to their more concise abbreviated forms."""
-PERSON_SHORTHAND = _coconut.dict(((1, "1st person"), (2, "2nd person"), (3, "3rd person")))  # type: Final[dict[int, str]]  #63: PERSON_SHORTHAND: Final[dict[int, str]] = {
-if "__annotations__" not in _coconut.locals():  #63: PERSON_SHORTHAND: Final[dict[int, str]] = {
-    __annotations__ = {}  # type: ignore  #63: PERSON_SHORTHAND: Final[dict[int, str]] = {
-__annotations__["PERSON_SHORTHAND"] = 'Final[dict[int, str]]'  #63: PERSON_SHORTHAND: Final[dict[int, str]] = {
+from aenum import Enum  #6: from aenum import Enum, MultiValue
+from aenum import MultiValue  #6: from aenum import Enum, MultiValue
 
 
-class EndingComponents(SimpleNamespace):  #70: class EndingComponents(SimpleNamespace):
+@_coconut_handle_cls_kwargs(settings=MultiValue, init="regular shorthand")  #9: class Number(Enum, settings=MultiValue, init="regular shorthand"):
+class Number(Enum):  #9: class Number(Enum, settings=MultiValue, init="regular shorthand"):
+    """Represents the grammatical number."""  #10:     """Represents the grammatical number."""
+
+    SINGULAR = "singular", "sg"  #12:     SINGULAR = "singular", "sg"
+    PLURAL = "plural", "pl"  #13:     PLURAL = "plural", "pl"
+
+
+_coconut_call_set_names(Number)  #16: class Tense(Enum, settings=MultiValue, init="regular shorthand"):
+@_coconut_handle_cls_kwargs(settings=MultiValue, init="regular shorthand")  #16: class Tense(Enum, settings=MultiValue, init="regular shorthand"):
+class Tense(Enum):  #16: class Tense(Enum, settings=MultiValue, init="regular shorthand"):
+    """Represents the tense of a verb."""  #17:     """Represents the tense of a verb."""
+
+    PRESENT = "present", "pre"  #19:     PRESENT = "present", "pre"
+    IMPERFECT = "imperfect", "imp"  #20:     IMPERFECT = "imperfect", "imp"
+    FUTURE = "future", "fut"  #21:     FUTURE = "future", "fut"
+    PERFECT = "perfect", "per"  #22:     PERFECT = "perfect", "per"
+    PLUPERFECT = "pluperfect", "plp"  #23:     PLUPERFECT = "pluperfect", "plp"
+# FUTURE_PERFECT = "future perfect", "fpr"
+
+
+_coconut_call_set_names(Tense)  #27: class Voice(Enum, settings=MultiValue, init="regular shorthand"):
+@_coconut_handle_cls_kwargs(settings=MultiValue, init="regular shorthand")  #27: class Voice(Enum, settings=MultiValue, init="regular shorthand"):
+class Voice(Enum):  #27: class Voice(Enum, settings=MultiValue, init="regular shorthand"):
+    """Represents the voice of a verb."""  #28:     """Represents the voice of a verb."""
+
+    ACTIVE = "active", "act"  #30:     ACTIVE = "active", "act"
+    PASSIVE = "passive", "pas"  #31:     PASSIVE = "passive", "pas"
+
+
+_coconut_call_set_names(Voice)  #34: class Mood(Enum, settings=MultiValue, init="regular shorthand"):
+@_coconut_handle_cls_kwargs(settings=MultiValue, init="regular shorthand")  #34: class Mood(Enum, settings=MultiValue, init="regular shorthand"):
+class Mood(Enum):  #34: class Mood(Enum, settings=MultiValue, init="regular shorthand"):
+    """Represents the mood of a verb."""  #35:     """Represents the mood of a verb."""
+
+    INDICATIVE = "indicative", "ind"  #37:     INDICATIVE = "indicative", "ind"
+    INFINITIVE = "infinitive", "inf"  #38:     INFINITIVE = "infinitive", "inf"
+    IMPERATIVE = "imperative", "ipe"  #39:     IMPERATIVE = "imperative", "ipe"
+    SUBJUNCTIVE = "subjunctive", "sbj"  #40:     SUBJUNCTIVE = "subjunctive", "sbj"
+    PARTICIPLE = "participle", "ptc"  #41:     PARTICIPLE = "participle", "ptc"
+
+
+_coconut_call_set_names(Mood)  #44: class Case(Enum, settings=MultiValue, init="regular shorthand"):
+@_coconut_handle_cls_kwargs(settings=MultiValue, init="regular shorthand")  #44: class Case(Enum, settings=MultiValue, init="regular shorthand"):
+class Case(Enum):  #44: class Case(Enum, settings=MultiValue, init="regular shorthand"):
+    """Represents the case of a noun."""  #45:     """Represents the case of a noun."""
+
+    NOMINATIVE = "nominative", "nom"  #47:     NOMINATIVE = "nominative", "nom"
+    VOCATIVE = "vocative", "voc"  #48:     VOCATIVE = "vocative", "voc"
+    ACCUSATIVE = "accusative", "acc"  #49:     ACCUSATIVE = "accusative", "acc"
+    GENITIVE = "genitive", "gen"  #50:     GENITIVE = "genitive", "gen"
+    DATIVE = "dative", "dat"  #51:     DATIVE = "dative", "dat"
+    ABLATIVE = "ablative", "abl"  #52:     ABLATIVE = "ablative", "abl"
+
+
+_coconut_call_set_names(Case)  #55: class Gender(Enum, settings=MultiValue, init="regular shorthand"):
+@_coconut_handle_cls_kwargs(settings=MultiValue, init="regular shorthand")  #55: class Gender(Enum, settings=MultiValue, init="regular shorthand"):
+class Gender(Enum):  #55: class Gender(Enum, settings=MultiValue, init="regular shorthand"):
+    """Represents the gender of a noun or adjective."""  #56:     """Represents the gender of a noun or adjective."""
+
+    MASCULINE = "masculine", "m"  #58:     MASCULINE = "masculine", "m"
+    FEMININE = "feminine", "f"  #59:     FEMININE = "feminine", "f"
+    NEUTER = "neuter", "n"  #60:     NEUTER = "neuter", "n"
+
+
+_coconut_call_set_names(Gender)  #63: class Degree(Enum, settings=MultiValue, init="regular shorthand"):
+@_coconut_handle_cls_kwargs(settings=MultiValue, init="regular shorthand")  #63: class Degree(Enum, settings=MultiValue, init="regular shorthand"):
+class Degree(Enum):  #63: class Degree(Enum, settings=MultiValue, init="regular shorthand"):
+    """Represents the degree of an adjective."""  #64:     """Represents the degree of an adjective."""
+
+    POSITIVE = "positive", "pos"  #66:     POSITIVE = "positive", "pos"
+    COMPARATIVE = "comparative", "cmp"  #67:     COMPARATIVE = "comparative", "cmp"
+    SUPERLATIVE = "superlative", "spr"  #68:     SUPERLATIVE = "superlative", "spr"
+
+
+_coconut_call_set_names(Degree)  #71: class Person(Enum, settings=MultiValue, init="regular shorthand"):
+@_coconut_handle_cls_kwargs(settings=MultiValue, init="regular shorthand")  #71: class Person(Enum, settings=MultiValue, init="regular shorthand"):
+class Person(Enum):  #71: class Person(Enum, settings=MultiValue, init="regular shorthand"):
+    """Represents the person of a verb."""  #72:     """Represents the person of a verb."""
+
+    FIRST = 1, "1st person"  #74:     FIRST = 1, "1st person"
+    SECOND = 2, "2nd person"  #75:     SECOND = 2, "2nd person"
+    THIRD = 3, "3rd person"  #76:     THIRD = 3, "3rd person"
+
+
+_coconut_call_set_names(Person)  #79: class EndingComponents(SimpleNamespace):
+class EndingComponents(SimpleNamespace):  #79: class EndingComponents(SimpleNamespace):
     """A container for the grammatical components of an ending.
 
     Examples
@@ -137,12 +161,12 @@ class EndingComponents(SimpleNamespace):  #70: class EndingComponents(SimpleName
                                number="singular")
     >>> foo.case
     'nominative'
-    """  #79:     """
+    """  #88:     """
 
 
-_coconut_call_set_names(EndingComponents)  #82: @dataclass(init=True)
-@dataclass(init=True)  #82: @dataclass(init=True)
-class MultipleMeanings(_coconut.object):  #83: class MultipleMeanings:
+_coconut_call_set_names(EndingComponents)  #91: @dataclass(init=True)
+@dataclass(init=True)  #91: @dataclass(init=True)
+class MultipleMeanings(_coconut.object):  #92: class MultipleMeanings:
     """Represents multiple meanings, with a main meaning and other meanings.
 
     Attributes
@@ -164,27 +188,27 @@ class MultipleMeanings(_coconut.object):  #83: class MultipleMeanings:
 
     >>> foo.__str__()
     'hide'
-    """  #105:     """
+    """  #114:     """
 
-    meanings = _coconut.typing.cast(_coconut.typing.Any, _coconut.Ellipsis)  # type: list[str]  #107:     meanings: list[str]
-    if "__annotations__" not in _coconut.locals():  #107:     meanings: list[str]
-        __annotations__ = {}  # type: ignore  #107:     meanings: list[str]
-    __annotations__["meanings"] = 'list[str]'  #107:     meanings: list[str]
+    meanings = _coconut.typing.cast(_coconut.typing.Any, _coconut.Ellipsis)  # type: list[str]  #116:     meanings: list[str]
+    if "__annotations__" not in _coconut.locals():  #116:     meanings: list[str]
+        __annotations__ = {}  # type: ignore  #116:     meanings: list[str]
+    __annotations__["meanings"] = 'list[str]'  #116:     meanings: list[str]
 
-    def __str__(self):  #109:     def __str__(self) -> str:
+    def __str__(self):  #118:     def __str__(self) -> str:
 # type: (...) -> str
-        return self.meanings[0]  #110:         return self.meanings[0]
+        return self.meanings[0]  #119:         return self.meanings[0]
 
 
-    @_coconut_tco  #112:     def __repr__(self) -> str:
-    def __repr__(self):  #112:     def __repr__(self) -> str:
+    @_coconut_tco  #121:     def __repr__(self) -> str:
+    def __repr__(self):  #121:     def __repr__(self) -> str:
 # type: (...) -> str
-        return _coconut_tail_call("MultipleMeanings({_coconut_format_0})".format, _coconut_format_0=(', '.join(self.meanings)))  #113:         return f"MultipleMeanings({', '.join(self.meanings)})"
+        return _coconut_tail_call("MultipleMeanings({_coconut_format_0})".format, _coconut_format_0=(', '.join(self.meanings)))  #122:         return f"MultipleMeanings({', '.join(self.meanings)})"
 
 
 
-_coconut_call_set_names(MultipleMeanings)  #116: class MultipleEndings(SimpleNamespace):
-class MultipleEndings(SimpleNamespace):  #116: class MultipleEndings(SimpleNamespace):
+_coconut_call_set_names(MultipleMeanings)  #125: class MultipleEndings(SimpleNamespace):
+class MultipleEndings(SimpleNamespace):  #125: class MultipleEndings(SimpleNamespace):
     """Represents multiple endings for a word, where each ending is a
     separate string.
 
@@ -211,10 +235,10 @@ class MultipleEndings(SimpleNamespace):  #116: class MultipleEndings(SimpleNames
 
     >>> foo.get_all()
     ['nostri', 'nostrum']
-    """  # noqa: D205  #143:     """  # noqa: D205
+    """  # noqa: D205  #152:     """  # noqa: D205
 
-    @_coconut_tco  #145:     def get_all(self) -> list[str]:
-    def get_all(self):  #145:     def get_all(self) -> list[str]:
+    @_coconut_tco  #154:     def get_all(self) -> list[str]:
+    def get_all(self):  #154:     def get_all(self) -> list[str]:
 # type: (...) -> list[str]
         """Returns a list of all the possible endings.
 
@@ -222,29 +246,29 @@ class MultipleEndings(SimpleNamespace):  #116: class MultipleEndings(SimpleNames
         -------
         list[str]
             The endings.
-        """  #152:         """
-        return _coconut_tail_call(list, self.__dict__.values())  #153:         return list(self.__dict__.values())
+        """  #161:         """
+        return _coconut_tail_call(list, self.__dict__.values())  #162:         return list(self.__dict__.values())
 
 
-    @_coconut_tco  #155:     def __str__(self) -> str:
-    def __str__(self):  #155:     def __str__(self) -> str:
+    @_coconut_tco  #164:     def __str__(self) -> str:
+    def __str__(self):  #164:     def __str__(self) -> str:
 # type: (...) -> str
-        return _coconut_tail_call("/".join, self.__dict__.values())  #156:         return "/".join(self.__dict__.values())
+        return _coconut_tail_call("/".join, self.__dict__.values())  #165:         return "/".join(self.__dict__.values())
 
 
-    def __add__(self, val2  # type: str  #158:     def __add__(self, val2: str) -> str:
-        ):  #158:     def __add__(self, val2: str) -> str:
+    def __add__(self, val2  # type: str  #167:     def __add__(self, val2: str) -> str:
+        ):  #167:     def __add__(self, val2: str) -> str:
 # type: (...) -> str
-        return self.__str__() + val2  #159:         return self.__str__() + val2
+        return self.__str__() + val2  #168:         return self.__str__() + val2
 
 # Allows for a prefix to be added to all of the endings.
 # pragma: no cover
-    @_coconut_tco  # pragma: no cover  #162:     def __radd__(self, val2: str) -> MultipleEndings:  # pragma: no cover
-    def __radd__(self, val2  # type: str  # pragma: no cover  #162:     def __radd__(self, val2: str) -> MultipleEndings:  # pragma: no cover
-        ):  # pragma: no cover  #162:     def __radd__(self, val2: str) -> MultipleEndings:  # pragma: no cover
+    @_coconut_tco  # pragma: no cover  #171:     def __radd__(self, val2: str) -> MultipleEndings:  # pragma: no cover
+    def __radd__(self, val2  # type: str  # pragma: no cover  #171:     def __radd__(self, val2: str) -> MultipleEndings:  # pragma: no cover
+        ):  # pragma: no cover  #171:     def __radd__(self, val2: str) -> MultipleEndings:  # pragma: no cover
 # type: (...) -> MultipleEndings  # pragma: no cover
-        prefixed = _coconut.dict(((key), ("{_coconut_format_0}{_coconut_format_1}".format(_coconut_format_0=(val2), _coconut_format_1=(value)))) for key, value in self.__dict__.items())  #163:         prefixed = {
-        return _coconut_tail_call(MultipleEndings, **prefixed)  #166:         return MultipleEndings(**prefixed)
+        prefixed = _coconut.dict(((key), ("{_coconut_format_0}{_coconut_format_1}".format(_coconut_format_0=(val2), _coconut_format_1=(value)))) for key, value in self.__dict__.items())  #172:         prefixed = {
+        return _coconut_tail_call(MultipleEndings, **prefixed)  #175:         return MultipleEndings(**prefixed)
 
 
-_coconut_call_set_names(MultipleEndings)  #168:         return MultipleEndings(**prefixed)
+_coconut_call_set_names(MultipleEndings)  #177:         return MultipleEndings(**prefixed)
