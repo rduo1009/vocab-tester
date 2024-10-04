7ceec3e6a5ebb45b198a6651ae23996bbcb1f676
rduo1009 <robertduong09alt@gmail.com>
2024-10-04 14:35:01 +1000

    🐛 fix: download wordnet if its not already there

--- python_src/transfero/synonyms.py
+++ python_src/transfero/synonyms.py
@@ -5,7 +5,14 @@
 
 from __future__ import annotations
 
+from nltk import download  # type: ignore[import-untyped]
 from nltk.corpus import wordnet  # type: ignore[import-untyped]
+from nltk.data import find  # type: ignore[import-untyped]
+
+try:
+    find("corpora/wordnet.zip")
+except LookupError:
+    download("wordnet")
 
 
 def find_synonyms(word: str) -> set[str]:
