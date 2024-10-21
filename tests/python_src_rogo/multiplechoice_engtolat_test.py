37d98de58a3d5efa46e201665f5367fffcc6ada3
rduo1009 <robertduong09alt@gmail.com>
2024-10-21 14:43:45 +1100

    🚨 tests: better tests for multiple choice questions

--- tests/python_src_rogo/multiplechoice_engtolat_test.py
+++ tests/python_src_rogo/multiplechoice_engtolat_test.py
@@ -5,8 +5,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.
 
 from pathlib import Path
 
+from python_src.accido.endings import Adjective, Noun, Pronoun, RegularWord, Verb
+from python_src.accido.misc import Gender
+from python_src.lego.misc import VocabList
 from python_src.lego.reader import read_vocab_file
 from python_src.rogo.asker import ask_question_without_sr
+from python_src.rogo.question_classes import MultipleChoiceEngToLatQuestion
 from python_src.rogo.type_aliases import Settings
 
 settings: Settings = {
@@ -107,4 +111,79 @@ def test_multiplechoice_engtolat():
     vocab_list = read_vocab_file(Path("tests/python_src_lego/test_vocab_files/regular_list.txt"))
     amount = 1000
     for output in ask_question_without_sr(vocab_list, amount, settings):
+        assert type(output) is MultipleChoiceEngToLatQuestion
+
         ic(output)  # type: ignore[name-defined] # noqa: F821
+
+
+def test_multiplechoice_engtolat_adjective():
+    word1 = Adjective("laetus", "laeta", "laetum", declension="212", meaning="happy")
+    word2 = Adjective("ingens", "ingentis", declension="3", termination=1, meaning="large")
+    word3 = Adjective("fortis", "forte", declension="3", termination=2, meaning="strong")
+    vocab_list = VocabList([word1, word2, word3])
+    amount = 1000
+
+    for output in ask_question_without_sr(vocab_list, amount, settings):
+        assert type(output) is MultipleChoiceEngToLatQuestion
+        assert output.answer in output.choices
+        assert set(output.choices) == {"laetus", "ingens", "fortis"}
+        assert (output.prompt, output.answer) in {("happy", "laetus"), ("large", "ingens"), ("strong", "fortis")}
+
+
+def test_multiplechoice_engtolat_noun():
+    word1 = Noun(nominative="puella", genitive="puellae", gender=Gender.FEMININE, meaning="girl")
+    word2 = Noun(nominative="servus", genitive="servi", gender=Gender.MASCULINE, meaning="slave")
+    word3 = Noun(nominative="canis", genitive="canis", gender=Gender.MASCULINE, meaning="dog")
+    vocab_list = VocabList([word1, word2, word3])
+    amount = 1000
+
+    for output in ask_question_without_sr(vocab_list, amount, settings):
+        assert type(output) is MultipleChoiceEngToLatQuestion
+        assert output.answer in output.choices
+        assert set(output.choices) == {"puella", "servus", "canis"}
+        assert (output.prompt, output.answer) in {("girl", "puella"), ("slave", "servus"), ("dog", "canis")}
+
+
+def test_multiplechoice_engtolat_pronoun():
+    word1 = Pronoun(pronoun="hic", meaning="this")
+    word2 = Pronoun(pronoun="ille", meaning="that")
+    word3 = Pronoun(pronoun="qui", meaning="who")
+    vocab_list = VocabList([word1, word2, word3])
+    amount = 1000
+
+    for output in ask_question_without_sr(vocab_list, amount, settings):
+        assert type(output) is MultipleChoiceEngToLatQuestion
+
+        assert output.answer in output.choices
+        assert set(output.choices) == {"hic", "ille", "qui"}
+        assert (output.prompt, output.answer) in {("this", "hic"), ("that", "ille"), ("who", "qui")}
+
+
+def test_multiplechoice_engtolat_verb():
+    word1 = Verb(present="doceo", infinitive="docere", perfect="docui", ppp="doctus", meaning="teach")
+    word2 = Verb(present="traho", infinitive="trahere", perfect="traxi", ppp="tractus", meaning="drag")
+    word3 = Verb(present="audio", infinitive="audire", perfect="audivi", ppp="auditus", meaning="hear")
+    vocab_list = VocabList([word1, word2, word3])
+    amount = 1000
+
+    for output in ask_question_without_sr(vocab_list, amount, settings):
+        assert type(output) is MultipleChoiceEngToLatQuestion
+
+        assert output.answer in output.choices
+        assert set(output.choices) == {"doceo", "traho", "audio"}
+        assert (output.prompt, output.answer) in {("teach", "doceo"), ("drag", "traho"), ("hear", "audio")}
+
+
+def test_multiplechoice_engtolat_regularword():
+    word1 = RegularWord(word="in", meaning="in")
+    word2 = RegularWord(word="e", meaning="out of")
+    word3 = RegularWord(word="post", meaning="after")
+    vocab_list = VocabList([word1, word2, word3])
+    amount = 1000
+
+    for output in ask_question_without_sr(vocab_list, amount, settings):
+        assert type(output) is MultipleChoiceEngToLatQuestion
+
+        assert output.answer in output.choices
+        assert set(output.choices) == {"in", "e", "post"}
+        assert (output.prompt, output.answer) in {("in", "in"), ("out of", "e"), ("after", "post")}
--- tests/python_src_rogo/multiplechoice_lattoeng_test.py
+++ tests/python_src_rogo/multiplechoice_lattoeng_test.py
@@ -5,8 +5,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../.
 
 from pathlib import Path
 
+from python_src.accido.endings import Adjective, Noun, Pronoun, RegularWord, Verb
+from python_src.accido.misc import Gender
+from python_src.lego.misc import VocabList
 from python_src.lego.reader import read_vocab_file
 from python_src.rogo.asker import ask_question_without_sr
+from python_src.rogo.question_classes import MultipleChoiceLattoEngQuestion
 from python_src.rogo.type_aliases import Settings
 
 settings: Settings = {
@@ -107,4 +111,79 @@ def test_multiplechoice_lattoeng():
     vocab_list = read_vocab_file(Path("tests/python_src_lego/test_vocab_files/regular_list.txt"))
     amount = 1000
     for output in ask_question_without_sr(vocab_list, amount, settings):
+        assert type(output) is MultipleChoiceLattoEngQuestion
+
         ic(output)  # type: ignore[name-defined] # noqa: F821
+
+
+def test_multiplechoice_engtolat_adjective():
+    word1 = Adjective("laetus", "laeta", "laetum", declension="212", meaning="happy")
+    word2 = Adjective("ingens", "ingentis", declension="3", termination=1, meaning="large")
+    word3 = Adjective("fortis", "forte", declension="3", termination=2, meaning="strong")
+    vocab_list = VocabList([word1, word2, word3])
+    amount = 1000
+
+    for output in ask_question_without_sr(vocab_list, amount, settings):
+        assert type(output) is MultipleChoiceLattoEngQuestion
+        assert output.answer in output.choices
+        assert set(output.choices) == {"happy", "large", "strong"}
+        assert (output.prompt, output.answer) in {("laetus", "happy"), ("ingens", "large"), ("fortis", "strong")}
+
+
+def test_multiplechoice_engtolat_noun():
+    word1 = Noun(nominative="puella", genitive="puellae", gender=Gender.FEMININE, meaning="girl")
+    word2 = Noun(nominative="servus", genitive="servi", gender=Gender.MASCULINE, meaning="slave")
+    word3 = Noun(nominative="canis", genitive="canis", gender=Gender.MASCULINE, meaning="dog")
+    vocab_list = VocabList([word1, word2, word3])
+    amount = 1000
+
+    for output in ask_question_without_sr(vocab_list, amount, settings):
+        assert type(output) is MultipleChoiceLattoEngQuestion
+        assert output.answer in output.choices
+        assert set(output.choices) == {"girl", "slave", "dog"}
+        assert (output.prompt, output.answer) in {("puella", "girl"), ("servus", "slave"), ("canis", "dog")}
+
+
+def test_multiplechoice_engtolat_pronoun():
+    word1 = Pronoun(pronoun="hic", meaning="this")
+    word2 = Pronoun(pronoun="ille", meaning="that")
+    word3 = Pronoun(pronoun="qui", meaning="who")
+    vocab_list = VocabList([word1, word2, word3])
+    amount = 1000
+
+    for output in ask_question_without_sr(vocab_list, amount, settings):
+        assert type(output) is MultipleChoiceLattoEngQuestion
+
+        assert output.answer in output.choices
+        assert set(output.choices) == {"this", "that", "who"}
+        assert (output.prompt, output.answer) in {("hic", "this"), ("ille", "that"), ("qui", "who")}
+
+
+def test_multiplechoice_engtolat_verb():
+    word1 = Verb(present="doceo", infinitive="docere", perfect="docui", ppp="doctus", meaning="teach")
+    word2 = Verb(present="traho", infinitive="trahere", perfect="traxi", ppp="tractus", meaning="drag")
+    word3 = Verb(present="audio", infinitive="audire", perfect="audivi", ppp="auditus", meaning="hear")
+    vocab_list = VocabList([word1, word2, word3])
+    amount = 1000
+
+    for output in ask_question_without_sr(vocab_list, amount, settings):
+        assert type(output) is MultipleChoiceLattoEngQuestion
+
+        assert output.answer in output.choices
+        assert set(output.choices) == {"teach", "drag", "hear"}
+        assert (output.prompt, output.answer) in {("doceo", "teach"), ("traho", "drag"), ("audio", "hear")}
+
+
+def test_multiplechoice_engtolat_regularword():
+    word1 = RegularWord(word="in", meaning="in")
+    word2 = RegularWord(word="e", meaning="out of")
+    word3 = RegularWord(word="post", meaning="after")
+    vocab_list = VocabList([word1, word2, word3])
+    amount = 1000
+
+    for output in ask_question_without_sr(vocab_list, amount, settings):
+        assert type(output) is MultipleChoiceLattoEngQuestion
+
+        assert output.answer in output.choices
+        assert set(output.choices) == {"in", "out of", "after"}
+        assert (output.prompt, output.answer) in {("in", "in"), ("out of", "e"), ("after", "post")}
