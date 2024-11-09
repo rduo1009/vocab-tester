c372f8c800ac83c97dca6fcc52614e66c1176e04
rduo1009 <robertduong09alt@gmail.com>
2024-11-09 18:54:03 +1100

    🧹 chore: bump version to 0.4.1-dev

--- python_src/__init__.py
+++ python_src/__init__.py
@@ -3,7 +3,7 @@
 from . import accido, lego, rogo, transfero
 
 __all__ = ["accido", "lego", "rogo", "transfero"]
-__version__ = "0.4-dev"
+__version__ = "0.4.1-dev"
 __author__ = "rduo1009"
 __copyright__ = "2024, rduo1009"
 __license__ = "MIT"
