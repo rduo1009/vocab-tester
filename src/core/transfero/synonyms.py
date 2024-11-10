35cc222f20855bd7cbcb512496730065ab56a762
rduo1009 <robertduong09alt@gmail.com>
2024-11-10 19:28:02 +1100

    🐛 fix: fixing bug with nltk dataset location

--- src/core/transfero/synonyms.py
+++ src/core/transfero/synonyms.py
@@ -9,7 +9,7 @@ from nltk import download
 from nltk.corpus import wordnet
 from nltk.data import find, path
 
-_project_root: Path = Path(__file__).parent.parent.parent
+_project_root: Path = Path(__file__).parent.parent.parent.parent
 _nltk_data_path: Path = _project_root / "nltk_data"
 _nltk_data_path.mkdir(parents=True, exist_ok=True)
 path.append(str(_nltk_data_path))
