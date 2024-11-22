2b9e40085d6f4c0ff657b6a9c0674f38ae031ede
rduo1009 <robertduong09alt@gmail.com>
2024-11-22 13:31:25 +1100

    🚧 build: dynamic versioning

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,12 @@
 """A wrapper for the Python packages used by vocab-tester."""
 
+import dunamai as _dunamai
+
 from . import core
 
-__version__ = "0.5.0-dev"
+__version__ = _dunamai.get_version(
+    "src", third_choice=_dunamai.Version.from_any_vcs
+).serialize()
 __author__ = "rduo1009"
 __copyright__ = "2024, rduo1009"
 __license__ = "MIT"
