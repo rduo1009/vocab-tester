6cf37569e3f3fa46f78c551370c8ad636979d1c3
rduo1009 <robertduong09alt@gmail.com>
2024-11-23 18:43:34 +1100

    🔨 refactor: adding `__all__` to utils init

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -6,3 +6,12 @@ from .duplicates import contains_duplicates as contains_duplicates
 from .duplicates import remove_duplicates as remove_duplicates
 from .set_functions import set_choice as set_choice
 from .set_functions import set_choice_pop as set_choice_pop
+
+__all__ = [
+    "compact",
+    "compare",
+    "contains_duplicates",
+    "remove_duplicates",
+    "set_choice",
+    "set_choice_pop",
+]
